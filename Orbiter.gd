6a76f32b17da2a5246c59bfe797840cda43daf72
quarke <manorris403@gmail.com>
2021-01-02 01:36:13 -0500

    first commit.

---
+++ Orbiter.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+export (int) var speed = 100
+var velocity = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+func _draw():
+	var color = Color("8bf1f3")
+	draw_circle(Vector2(0,0),10.0,color)
+#func set_move():
+#	velocity = Vector2(0,0)
+#	var my_pos = global_position
+#	var mouse_pos = get_global_mouse_position()
+#	if my_pos.x > mouse_pos.x:
+#		velocity.x -= 1
+#	elif my_pos.x < mouse_pos.x:
+#		velocity.x += 1
+#	if my_pos.y > mouse_pos.y:
+#		velocity.y -= 1
+#	elif my_pos.y < mouse_pos.y:
+#		velocity.y += 1
+#	velocity = velocity.normalized() * speed
+
+func set_move():
+	#rotation = get_global_mouse_position().angle_to_point(position)
+	look_at(get_global_mouse_position())
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_down"):
+		velocity = Vector2(0, -speed).rotated(rotation)
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2(0, speed).rotated(rotation)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	set_move()
+	velocity = move_and_slide(velocity)
---
+++ Orbiter_Collision.gd
@@ -0,0 +1,8 @@
+extends CollisionShape2D
+
+
+func _ready():
+	shape.radius = 10.0
+
+#func _process(delta):
+#	pass
